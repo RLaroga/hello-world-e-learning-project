114bb6cc5f295eaa3443b8caafc14582fad69750
Robert <robertfeiwin@gmail.com>
2024-05-27 17:43:26 +0800

    Change resultType to resultMap

--- src/main/java/com/helloWorld/helloWorld/HelloWorldApplication.java
+++ src/main/java/com/helloWorld/helloWorld/HelloWorldApplication.java
@@ -1,10 +1,8 @@
 package com.helloWorld.helloWorld;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.helloWorld.helloWorld.mapper")
 @SpringBootApplication(scanBasePackages = {"com.helloWorld", "com.helloWorld.helloWorld.mapper"})
 public class HelloWorldApplication {
 
