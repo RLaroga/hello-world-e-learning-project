1c78dd23179c245fa09c54d256448bbeec911717
Robert <robertfeiwin@gmail.com>
2024-06-11 17:22:27 +0800

    Add query for user registration

--- src/main/java/com/helloWorld/helloWorld/mapper/UserMapper.java
+++ src/main/java/com/helloWorld/helloWorld/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.helloWorld.helloWorld.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.helloWorld.helloWorld.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -10,5 +11,9 @@ import java.util.List;
 public interface UserMapper extends BaseMapper<User> {
 
     List<User> selectUserList();
+
+    User findByUsername(@Param("username") String username);
+
+    int insert(@Param("user") User user);
 
 }
