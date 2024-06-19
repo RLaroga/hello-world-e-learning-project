828e328dc778aad9701674fb6be7df107adb2ae7
Robert <robertfeiwin@gmail.com>
2024-06-19 17:50:29 +0800

    [AOP] Test the implementation of aspect oriented programming
    Signed-off-by: Robert <robertfeiwin@gmail.com>

---
+++ src/main/java/com/helloWorld/helloWorld/mainStudy/aspect/dto/Comment.java
@@ -0,0 +1,14 @@
+package com.helloWorld.helloWorld.mainStudy.aspect.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Comment {
+
+    private String text;
+
+    private String author;
+
+}
