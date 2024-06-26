e2adb0fd3017ae03e762673354e11969d7cd6d5f
Robert <robertfeiwin@gmail.com>
2024-06-26 09:59:47 +0800

    [AOP] Test aspect oriented programming using @ annotation

--- src/main/java/com/helloWorld/helloWorld/mainStudy/aspect/service/CommentService.java
+++ src/main/java/com/helloWorld/helloWorld/mainStudy/aspect/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.helloWorld.helloWorld.mainStudy.aspect.service;
 
+import com.helloWorld.helloWorld.mainStudy.aspect.annotation.ToLog;
 import com.helloWorld.helloWorld.mainStudy.aspect.dto.Comment;
 import org.springframework.stereotype.Service;
 
@@ -7,10 +8,22 @@ import java.util.logging.Logger;
 
 @Service
 public class CommentService {
-    private Logger logger = Logger.getLogger(CommentService.class.getName());
+    private final Logger logger = Logger.getLogger(CommentService.class.getName());
 
+    @ToLog
     public String publishComment(Comment comment) {
         logger.info("Publishing comment:" + comment.getText());
         return "SUCCESS";
     }
+
+    @ToLog
+    public String deleteComment(Comment comment) {
+        logger.info("Deleting comment: " + comment.getText());
+        return "DELETE COMMENT";
+    }
+
+    public void editComment(Comment comment) {
+        logger.info("Editing comment: " + comment.getText());
+    }
+
 }
